bb533ce0c5b23f5919bf0f9752d79f191900f0a6
Adam Lahjouji <adamlahjouji@rocketmail.com>
2019-10-28 15:27:34 -0400

    puts data from sql to frontend

--- routes/eventList.js
+++ routes/eventList.js
@@ -3,38 +3,11 @@ var router = express.Router();
 
 /* GET Event List page. */
 router.get('/', function(req, res, next) {
-    
-    /* TEMPORARY HARDCODED DATA */
-    const events = [
-        {
-            name: "Border Collie Festival",
-            university: "University of Central Florida",
-            eventid: "event01",
-            time: "10-10-2020"
-        },
-        {
-            name: "Caturday",
-            university: "University of South Florida",
-            eventid: "event02",
-            time: "10-10-2020"
-        },
-        {
-            name: "Movie Night",
-            university: "Florida State University",
-            eventid: "event03",
-            time: "10-10-2020"
-        },
-        {
-            name: "Crippling Debt Day",
-            university: "Full Sail University",
-            eventid: "event04",
-            time: "10-10-2020"
-        }
-    ];
-    /* END TEMPORARY HARDCODED DATA */
+    var eventsList = [];
 
-    res.render('eventList', { 
-        events:events
+    dbConnection.query("SELECT * FROM Events", (err, rows) => {
+      if (err) throw err;
+      res.render('eventList', { events: rows });
     });
 });
 
