f3931c54870715940000ec9e389d4af4176dad51
Adam Lahjouji <adamlahjouji@rocketmail.com>
2019-10-30 18:54:14 -0400

    added functionality

--- routes/createEvent.js
+++ routes/createEvent.js
@@ -9,4 +9,35 @@ router.get('/', function(req, res, next) {
     res.render('createEvent', { apiKey:apiKey });
 });
 
+
+
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+  const eventName = req.body.eventName;
+  const location = req.body.locName;
+  const eventCategory = req.body.eventCategory;
+  const description = req.body.eventDes;
+  const Time = req.body.eventTime;
+  const theDate = req.body.eventDate;
+  const dateTime = theDate + Time;
+  const contactPhone = req.body.phoneNum;
+  const contactEmail = req.body.email;
+  const schoolId = 666;
+  const schoolName = "WE MUST DO SOMETHIGN HERE";
+  const eventVisibility = req.body.eventVisibility;
+
+  console.log(eventCategory);
+
+  const query =
+      "INSERT INTO Events " +
+      "(eventName, location, eventCategory, description, dateTime, contactPhone, contactEmail, schoolId, schoolName, eventVisibility) " +
+      `VALUES ("${eventName}", "${location}", "${eventCategory}", "${description}", "${dateTime}", "${contactPhone}", "${contactEmail}", "${schoolId}", "${schoolName}", "${eventVisibility}");`;
+
+  dbConnection.query(query, (err, rows) => {
+    if (err) throw err;
+
+    res.render('createEvent');
+  });
+});
+
 module.exports = router;
