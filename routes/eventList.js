5f526b97a503e0ec6dcfc57ad6bfb57a56dcafde
Autumn Esponda <autumnskies1096@gmail.com>
2019-10-29 21:29:29 -0400

    fixed another bug in eventList accordions; added sql call to pass comments to eventlist frontend; updated .gitignore

--- routes/eventList.js
+++ routes/eventList.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET Event List page. */
 router.get('/', function(req, res, next) {
-    var eventsList = [];
-
     dbConnection.query("SELECT * FROM Events", (err, rows) => {
       if (err) throw err;
-      res.render('eventList', { events: rows });
+
+      const events = rows;
+      dbConnection.query("SELECT * FROM Comments", (err, rows) => {
+        if (err) throw err;
+
+        res.render('eventList', { events: events, comments: rows });
+      });
     });
 });
 
