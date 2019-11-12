0f757b2fa03d2768d8411739668cc372262d38e8
Autumn Esponda <autumnskies1096@gmail.com>
2019-11-12 14:43:53 -0500

    fixed lots of bugs in createEvent;

--- routes/createEvent.js
+++ routes/createEvent.js
@@ -10,23 +10,19 @@ router.get('/', function(req, res, next) {
     return;
   }
 
-  var apiKey = dbCredentials.google_api_key;
+  const apiKey = dbCredentials.google_api_key;
+  const uniId = req.session.universityId;
 
-  dbConnection.query("SELECT * FROM University", (err, universities) => {
+  dbConnection.query(`SELECT * FROM Location WHERE universityId = ${uniId}`, (err, locations) => {
     if (err) throw err;
 
-    dbConnection.query("SELECT * FROM Location", (err, locations) => {
-      if (err) throw err;
-
-      dbConnection.query("SELECT * FROM RSO WHERE RSO.isActive", (err, rsos) => {
-        res.render('createEvent', {
-          apiKey: apiKey,
-          universities: universities,
-          locations: locations,
-          activeRSO: rsos,
-          isAdmin: req.session.isAdmin,
-          isSuperAdmin: req.session.isSuperAdmin
-        });
+    dbConnection.query(`SELECT * FROM RSO WHERE RSO.isActive AND universityId = ${uniId}`, (err, rsos) => {
+      res.render('createEvent', {
+        apiKey: apiKey,
+        locations: locations,
+        activeRSO: rsos,
+        isAdmin: req.session.isAdmin,
+        isSuperAdmin: req.session.isSuperAdmin
       });
     });
   });
@@ -35,31 +31,20 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/', (req, res, next) => {
-  console.log(req.body);
   const eventName = req.body.eventName;
-
-  // const location = req.body.locName;
-  // TODO: CONVERT THE ABOVE INTO AN ID OR SOMETHING OOF
-  //  I'M SORRY THE LOCATION NAME MIGHT NOT BE UNIQUE
-  //  an idea would be to make the value attr for each
-  //  dropdown item be the id of the location instead of the name
-  const locationId = 666;
-
+  const locationId = req.body.locName;
   const eventCategory = req.body.eventCategory;
   const description = req.body.eventDes;
-  const Time = req.body.eventTime;
-  const theDate = req.body.eventDate;
-  const dateTime = theDate + " " + Time;
+  const eventDateTime = req.body.eventDate + " " + req.body.eventTime + ":00";
   const contactPhone = req.body.phoneNum;
   const contactEmail = req.body.email;
-  const schoolId = 666;
-  const schoolName = "WE MUST DO SOMETHIGN HERE";
+  const schoolId = req.session.universityId;
   const eventVisibility = req.body.eventVisibility;
 
   const query =
       "INSERT INTO Event " +
-      "(eventName, locationId, eventCategory, description, dateTime, contactPhone, contactEmail, schoolId, schoolName, eventVisibility) " +
-      `VALUES ("${eventName}", "${locationId}", "${eventCategory}", "${description}", "${dateTime}", "${contactPhone}", "${contactEmail}", "${schoolId}", "${schoolName}", "${eventVisibility}");`;
+      "(eventName, locationId, eventCategory, description, dateTime, contactPhone, contactEmail, schoolId, eventVisibility) " +
+      `VALUES ("${eventName}", "${locationId}", "${eventCategory}", "${description}", "${eventDateTime}", "${contactPhone}", "${contactEmail}", "${schoolId}", "${eventVisibility}");`;
 
   dbConnection.query(query, (err, rows) => {
     if (err) throw err;
